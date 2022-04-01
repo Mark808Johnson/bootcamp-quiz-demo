9e173a8e40392713634f7479281eb48421ab76b6
Mark Johnson <mark808johnson@gmail.com>
2022-04-01 15:01:42 +0300

    created questions file

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
-from tkinter.messagebox import QUESTION
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from questions import random_questions 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///test.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
-class Quiz(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(400), nullable=False)
-    option_a = db.Column(db.String(100), nullable=False)
-    option_b = db.Column(db.String(100), nullable=False)
-    option_c = db.Column(db.String(100), nullable=False)
-    answer = db.Column(db.String(100), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+# class Quiz(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     question = db.Column(db.String(400), nullable=False)
+#     option_a = db.Column(db.String(100), nullable=False)
+#     option_b = db.Column(db.String(100), nullable=False)
+#     option_c = db.Column(db.String(100), nullable=False)
+#     answer = db.Column(db.String(100), nullable=False)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
+
+@app.route("/")
+def quiz():
+    random_selection = random_questions()
+    for item in random_selection:
+    return render_template("index.html")    
+        
+@app.route("/results", methods=["POST"]
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ questions.py
@@ -0,0 +1,34 @@
+import random
+
+def random_questions():
+    quiz_questions = [
+        {
+            "question": "In which continent are Chile, Argentina and Brazil?",
+            "options": ["North America", "South America", "Europe"],
+            "answer": "South America"
+        },
+        {
+            "question": "Which brand of soup featured in one of Andy Warhol's most famous pop art pieces?",
+            "options": ["Heinz", "Campbell's", "Baxters"],
+            "answer": "Campbell's"
+
+        },
+        {
+            "question": "The Mad Hatter and the Cheshire Cat are characters in which famous book?",
+            "options": ["Winne-the-Pooh", "Charlotte's Web", "Alice in Wonderland"],
+            "answer": "Alice in Wonderland"
+        },
+        {
+            "question": "What measurement scale is used to determine wind speed?",
+            "options": ["Beaufort scale", "Richter scale", "Synoptic scale"],
+            "answer": "Beaufort scale"
+        },
+        {
+            "question": "In which city were the 1992 Summer Olympics held?",
+            "options": ["Seoul", "Sydney", "Barcelona"],
+            "answer": "Barcelona"
+        }
+    ]
+    random.shuffle(quiz_questions)
+    number_questions = 3
+    return quiz_questions[0:number_questions]
