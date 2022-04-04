66ff63b4d3d952227bcb16e97fe7cd3fbfe47dd6
Mark Johnson <mark808johnson@gmail.com>
2022-04-04 16:13:24 +0300

    created basic login

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
+from unicodedata import name
+from flask import Flask, render_template, request, url_for, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, time
 from questions import selected_quiz, num_questions
 
+
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///test.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
+app.config["SECRET_KEY"] = 'bootcamp'
 
 # class Quiz(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
@@ -21,20 +21,23 @@ app = Flask(__name__)
 #         return '<Task %r>' % self.id
 
 
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-#     if request.method == "POST":
-#         password = request.form.get("password")
-#         if password == "bootcamp":
-#              return render_template("quiz.html")     
-#         else:
-#             return render_template("login.html")
-#     else:
-#         return render_template("login.html")
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template('login.html')
+    
+@app.route("/quiz", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        typed_password = request.form["password"]
+        if typed_password == "bootcamp":
+            session['logged in'] = True
+            return render_template("quiz.html", selected_quiz=selected_quiz, num_questions=num_questions)
+        else:
+            return render_template('login.html')
 
-@app.route("/", methods=["GET"])
-def quiz():
-    return render_template("quiz.html", selected_quiz=selected_quiz, num_questions=num_questions)    
+# @app.route("/quiz", methods=["GET"])
+# def quiz():
+#     return render_template("quiz.html", selected_quiz=selected_quiz, num_questions=num_questions)    
 
 @app.route("/results", methods=["GET", "POST"])
 def quiz_results():
@@ -44,12 +47,10 @@ def quiz_results():
             answer = request.form.get(f"{item['q_id']}")
             if answer == item["answer"]:
                 correct_answers += 1
-        
-    
-    
-    
-    
-    return render_template("results.html",correct_answers=correct_answers, num_questions=num_questions)
+        name = request.form["name"]
+    else:
+        return redirect("quiz.html")
+    return render_template("results.html",correct_answers=correct_answers, num_questions=num_questions, name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ db/__init__.py
@@ -0,0 +1,15 @@
+# import sqlalchemy
+# from sqlalchemy.orm import sessionmaker
+# from dotenv import load_dotenv, find_dotenv
+# load_dotenv(find_dotenv())
+# import os
+
+# engine = sqlalchemy.create_engine(os.environ.get('DATABASE'))
+# Session = sessionmaker(bind=engine)
+
+
+       
+#         app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///leaderboard.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+       
